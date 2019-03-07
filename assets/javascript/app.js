ab0c63e92a10dc69e2b283c7df0ac49bc1f149c5
Cameron <cameronjmaccabe@gmail.com>
2019-03-06 22:28:10 -0800

    added trivia questions and media query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,26 +11,26 @@ $(document).ready(function() {
 
     function reset() {
         trivia = [
-            ["1", ["1", "b", "c", "d"], "1"],
-            ["2", ["a", "2", "c", "d"], "2"],
-            ["3", ["a", "b", "3", "d"], "3"],
-            ["4", ["a", "b", "c", "4"], "4"],
-            ["5", ["a", "b", "c", "d"], "a"],
-            ["6", ["1", "b", "c", "d"], "1"],
-            ["7", ["a", "2", "c", "d"], "2"],
-            ["8", ["a", "b", "3", "d"], "3"], 
-            ["9", ["a", "b", "c", "4"], "4"], 
-            ["10", ["a", "b", "c", "d"], "a"],
-            ["11", ["1", "b", "c", "d"], "1"],
-            ["12", ["a", "2", "c", "d"], "2"], 
-            ["13", ["a", "b", "3", "d"], "3"], 
-            ["14", ["a", "b", "c", "4"], "4"], 
-            ["15", ["a", "b", "c", "d"], "a"],
-            ["16", ["1", "b", "c", "d"], "1"], 
-            ["17", ["a", "2", "c", "d"], "2"], 
-            ["18", ["a", "b", "3", "d"], "3"], 
-            ["19", ["a", "b", "c", "4"], "4"], 
-            ["20", ["a", "b", "c", "d"], "a"]
+            ["Which of the following is not one of New Orleans's Mardi Gras colors?", ["green", "gold", "blue", "purple"], "blue"],
+            ["What is Mardi Gras known as in the Christian calendar?", ["Ash Tuesday", "Shrove Tuesday", "Fat Tuesday", "Good Tuesday"], "Shrove Tuesday"],
+            ["In what year was the first Mardi Gras celebration in North America held?", ["1697", "1703", "1715", "1726"], "1703"],
+            ["Where did the tradition of Mardi Gras in America begin?", ["New Orleans, LA", "Memphis, TN", "Mobile, AL", "Baton Rouge, LA"], "Mobile, AL"],
+            ["The first Mardi Gras parade held in New Orleans is recorded to have taken place in what year?", ["1737", "1792", "1815", "1837"], "1837"],
+            ["Which of these relatively small countries features one of the largest Carnival celebrations in the world?", ["Jamaica", "Trinidad and Tobago", "Grenada", "Barbados"], "Trinidad and Tobago"],
+            ["What did the Mardi Gras colors of New Orleans originally represent?", ["the American flag", "the French imperial navy", "the Russian royal family", "Cajun cultural traditions"], "the Russian royal family"],
+            ["Which of the following is not a traditional part of Mardi Gras in New Orleans?", ["beads", "king cake", "parade floats", "eating contests"], "eating contests"], 
+            ["Many Mardi Gras events are organized by private clubs known as _____.", ["krewes", "companies", "brotherhoods", "orders"], "krewes"], 
+            ["What is hidden inside a king cake?", ["a necklace", "a coin", "a doll", "a plastic baby"], "a plastic baby"],
+            ["The Mardi Gras parade season in New Orleans sometimes overlaps with what other annual event?", ["the World Series", "the Super Bowl", "Groundhog Day", "St. Patrick's Day"], "the Super Bowl"],
+            ["Which of the following are not commonly thrown at Mardi Gras parades?", ["stuffed animals", "beads", "doubloons", "hacky sacks"], "hacky sacks"], 
+            ["Which of the following symbols is associated with Mardi Gras and with Louisiana culture more generally?", ["fleur-de-lis", "caduceus", "Gallic rooster", "ichthys"], "fleur-de-lis"],
+            ["How many times has Mardi Gras been cancelled in New Orleans, as of 2019?", ["1", "5", "9", "13"], "13"],
+            ["Which of the following has not been a major cultural influence on the history of Mardi Gras?", ["Hollywood", "French culture", "paganism", "the Catholic Church"], "Hollywood"],
+            ["The precise date of Mardi Gras, which varies from year, is determined by _____.", ["lottery", "astrology", "Easter", "the Pope"], "Easter"],
+            ["Which of the following events resulted in the cancellation of Mardi Gras festivities in New Orleans?", ["World War II", "Prohibition", "The Great Depression", "Hurricane Katrina"], "World War II"],
+            ["The Carnival season begins on _____.", ["Pentecost", "Christmas", "Epiphany", "Mardi Gras"], "Epiphany"],
+            ["The king cake is named in reference to whom?", ["the biblical Magi", "Louis XIV", "Charlemagne", "the Russian royal family"], "the biblical Magi"],
+            ["Who was the first King of Mardi Gras?", ["Andrew Jackson", "Alexei Alexandrovich", "Abraham Lincoln", "Jean-Baptiste Le Moyne de Bienville"], "Alexei Alexandrovich"]
         ];
         time = 15;
         correct = 0;
